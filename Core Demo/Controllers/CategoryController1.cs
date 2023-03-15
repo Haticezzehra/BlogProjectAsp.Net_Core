10c4ec9a819d185697fa93c0782babe6a4b7d479
example <haticezehraorhan@gmail.com>
2023-03-15 12:09:42 +0300

    View

---
+++ Core Demo/Controllers/CategoryController1.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Demo.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+
+        public IActionResult Index()
+        {
+            var values= cm.GetAll();
+            return View(values);
+        }
+    }
+}
--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        public void Add(T entity)
+        {
+            using var c = new Context();
+            c.Add(entity);
+            c.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            using var c = new Context();
+            c.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+
+        }
+
+        public void Update(T entity)
+        {
+            using var c = new Context();
+            c.Update(entity);
+            c.SaveChanges();
+        }
     }
 }

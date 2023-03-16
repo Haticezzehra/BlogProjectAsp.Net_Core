c09f3aa30c936e6f480a7a34d4a7886b4895ccf0
example <haticezehraorhan@gmail.com>
2023-03-16 17:32:42 +0300

    component

--- Core Demo/ViewComponents/Comment/CommentListByBlog.cs
+++ Core Demo/ViewComponents/Comment/CommentListByBlog.cs
@@ -1,6 +1,16 @@
-﻿namespace Core_Demo.ViewComponents.Comment
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Demo.ViewComponents.Comment
 {
-    public class CommentListByBlog
+    public class CommentListByBlog : ViewComponent
     {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = commentManager.GetAll(8);
+            return View(values);
+        }
     }
 }

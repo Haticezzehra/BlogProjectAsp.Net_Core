ad8f6c74cd0a19ab3c18b38d99e36769334d8809
example <haticezehraorhan@gmail.com>
2023-03-30 09:19:05 +0300

    Admin

--- Core Demo/Areas/Admin/Controllers/CategoryController.cs
+++ Core Demo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,9 +1,15 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Reflection.Metadata;
 using X.PagedList;
 
-namespace Core_Demo.Areas.Admin.Controllers
+namespace Core_Demo.Areas.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
@@ -14,6 +20,46 @@ namespace Core_Demo.Areas.Admin.Controllers
         {
             var values = categoryManager.GetAll().ToPagedList(page, 3);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator wv = new CategoryValidator();
+            ValidationResult result = wv.Validate(category);
+            Category category1 = new Category();
+            // if (result.IsValid)
+            // {
+            category1.CategoryName = category.CategoryName;
+            category1.CategoryDescription = category.CategoryDescription;
+            category1.CategoryStatus = true;
+            categoryManager.Add(category1);
+            return RedirectToAction("Category", "Admin");
+
+            //  }
+            // else
+            // {
+
+
+            //  foreach (var item in result.Errors)
+            // {
+            //ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            // }
+            //  }
+            // return View();
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = categoryManager.GetById(id);
+            categoryManager.Delete(value);
+            return RedirectToAction("Category", "Admin");
         }
     }
 }

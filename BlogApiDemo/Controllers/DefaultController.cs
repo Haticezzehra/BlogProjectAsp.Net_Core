016a030cb02771236c57c47af07a2ff2b0ca1380
example <haticezehraorhan@gmail.com>
2023-03-31 17:52:46 +0300

    Api

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApiDemo.Controllers
+{
+    //Api Controller
+    [Route("api/[controller]")]  //bir web projesinden çağırmak istersek bu yolu yazıyoruz
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet] //Veri getireceğimiz kısım.
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Emplooyees.ToList();
+            return Ok(values); //Ok başarılı durum kodu demek.
+        }
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=CoreBlogDBApi;integrated security=true");
+        }
+        public DbSet<Employee> Emplooyees { get; set; }
+    }
+}

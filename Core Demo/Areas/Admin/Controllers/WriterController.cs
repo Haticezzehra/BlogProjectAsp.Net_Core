ae4e8e6f3898325c83000a34a5c45eeb9d4fdaf4
example <haticezehraorhan@gmail.com>
2023-03-31 15:46:35 +0300

    ~~ajax~~

--- Core Demo/Areas/Admin/Controllers/WriterController.cs
+++ Core Demo/Areas/Admin/Controllers/WriterController.cs
@@ -17,6 +17,34 @@ namespace Core_Demo.Areas.Admin.Controllers
             return Json(JsonWriter);
 
         }
+        public IActionResult GetWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+        [HttpPost]
+        public IActionResult AddWriter(WritterClass writer)
+        {
+            writers.Add(writer);
+            var jsonWriters = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriters);
+        }
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        public IActionResult UpdateWriter(WritterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+
         public static List<WritterClass> writers = new List<WritterClass>
         {
             new WritterClass

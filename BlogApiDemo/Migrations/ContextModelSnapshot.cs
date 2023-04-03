f6a8760e474699d109d8ee59056a83f26f1681ca
example <haticezehraorhan@gmail.com>
2023-04-03 16:26:59 +0300

    Identity

--- BlogApiDemo/Migrations/ContextModelSnapshot.cs
+++ BlogApiDemo/Migrations/ContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace BlogApiDemo.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.4")
+                .HasAnnotation("ProductVersion", "7.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
